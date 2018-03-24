8817dcb7699182f1ce9495a9ae286e4da6d479c4
Devon Bull <dbull@live.com>
2018-03-24 16:57:40 -0700

    changed public path for new domain name

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/crxsolutions/',
+    assetsPublicPath: 'https://crxsolutions.io/',
 
     /**
      * Source Maps
