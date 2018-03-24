464bd57b22052f3e20ebba0b08deff7054a0fedc
Devon Bull <dbull@live.com>
2018-03-24 16:39:58 -0700

    everything loaded except img

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/crxsolutions',
+    assetsPublicPath: '/crxsolutions/',
 
     /**
      * Source Maps
