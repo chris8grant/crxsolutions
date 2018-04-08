a21f5eb7ccc84d97e0798fd972b5505af5bfd74b
Devon Bull <dbull@live.com>
2018-04-07 22:42:39 -0700

    added team section, removed scrollspy

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,5 @@
 <template>
-  <main data-spy="scroll" data-target="#navbar" data-offset="0">
+  <main>
     <div class="container p-5" id="about">
       <!-- <h2>About</h2> -->
       <div class="container">
@@ -10,7 +10,7 @@
     </div>
     <div class="jumbotron jumbotron-fluid overflow-scroll" id="solution">
       <div class="container-fluid"> 
-        <h2 class="text-white">How it Works</h2>
+        <h2 class="text-white pb-3">How it Works</h2>
         <!-- <p class="text-white mb-5 lead">Our product is designed to connect your funding needs with banks that are genuinely interested in supporting you. </p> -->
         <div class="row">
           <div class="col mb-3">
@@ -34,11 +34,38 @@
         </div>
       </div>
     </div>
-    <div class="container-fluid" id="story">
+    <div class="container-fluid p-2" id="story">
       <div class="container p-5">
         <h2 class="t">Our Story</h2>
         <p class="pt-4 lead">CRX launched in 2018 to increase access to capital and credit in low and moderate-income communities. Today, we've built world-class technology that connects banks with impactful community development projects that meet their Community Reinvestment Act compliance obligations. CRX is fundamentally transforming the way community development projects are financed and building the financial marketplace of the future. 
         </p>
+      </div>
+    </div>
+    <div class="container-fluid p-2 pt-5" id="team">
+      <h2 class="t pb-3">Our Team</h2>
+      <div class="container">
+        <div class="row team-row">
+          <div class="col-xs-3">
+            <img src="../assets/chris-profile.jpeg" width="200" height="200" class="profile"/>
+            <h5 class="pt-3">Chris</h5>
+            <p>CEO</p>
+          </div>
+          <div class="col-xs-3">
+            <img src="../assets/devon-profile.jpeg" width="200" height="200" class="profile"/>
+            <h5 class="pt-3">Devon</h5>
+            <p>Engineering</p>
+          </div>
+          <div class="col-xs-3">
+            <img src="../assets/sara-profile.jpeg" width="200" height="200" class="profile"/>
+            <h5 class="pt-3">Sara</h5>
+            <p>Business Development</p>
+          </div>
+          <div class="col-xs-3">
+            <img src="../assets/kate-profile.png" width="200" height="200" class="profile"/>
+            <h5 class="pt-3">Kate</h5>
+            <p>Operations</p>
+          </div>
+        </div>
       </div>
     </div>
   </main>
@@ -52,6 +79,14 @@ export default {
 </script>
 
 <style scoped>
+.team-row {
+  justify-content: space-around!important;
+}
+.profile {
+  border-radius: 5px;
+  /* filter: grayscale(100%); */
+}
+
 .overflow-scroll {
   overflow: auto;
 }
@@ -88,6 +123,11 @@ export default {
   margin-bottom: 0px;
   background-attachment: fixed;
 }
+
+#team {
+  background: #ece8e8;
+}
+
 @media (max-width: 575.98px) {
   .jumbotron {
     height: 170vh;
